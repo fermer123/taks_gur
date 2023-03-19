c0f47fe0810295f93d54d3f12521a5663c7e8027
fermer123 <fermer_525@mail.ru>
2023-03-19 20:52:28 +0500

    b4 skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import {FC} from 'react';
 import {Route, Routes} from 'react-router-dom';
+import Header from './components/component/Header/Header';
 import ItemId from './components/Pages/ItemId/ItemId';
 import Items from './components/Pages/Items/Items';
 
 const App: FC = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Items />} />
-      <Route path='/port/:id' element={<ItemId />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Items />} />
+        <Route path='/port/:id' element={<ItemId />} />
+      </Routes>
+    </>
   );
 };
 export default App;
---
+++ src/components/component/Header/Header.tsx
@@ -0,0 +1,111 @@
+import {Box} from '@mui/system';
+import {FC} from 'react';
+import styled from 'styled-components';
+
+const HeaderContainer = styled(Box)`
+  max-width: 1280px;
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 35px;
+`;
+
+const ButtonGroup = styled(Box)`
+  padding: 35px 0 26px 0;
+  display: flex;
+  gap: 16px;
+`;
+
+const StyledIcon = styled.svg`
+  width: 31px;
+  height: 31px;
+  &:hover {
+    fill: #a4a4a4;
+    cursor: pointer;
+  }
+  &:active {
+    fill: #00a0ab;
+  }
+`;
+
+const Header: FC = () => {
+  return (
+    <HeaderContainer>
+      <ButtonGroup>
+        <button type='button'>
+          <StyledIcon
+            width='31'
+            height='31'
+            viewBox='0 0 31 31'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
+            <rect
+              x='1.1'
+              y='1.1'
+              width='11.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+            <rect
+              x='18.1'
+              y='1.1'
+              width='11.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+            <rect
+              x='1.1'
+              y='18.1'
+              width='11.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+            <rect
+              x='18.1'
+              y='18.1'
+              width='11.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+          </StyledIcon>
+        </button>
+        <button type='button'>
+          <StyledIcon
+            width='31'
+            height='31'
+            viewBox='0 0 31 31'
+            fill='none'
+            xmlns='http://www.w3.org/2000/svg'>
+            <rect
+              x='1.1'
+              y='18.1'
+              width='28.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+            <rect
+              x='1.1'
+              y='1.1'
+              width='28.8'
+              height='11.8'
+              rx='1.9'
+              stroke='#C7C7C7'
+              strokeWidth='2.2'
+            />
+          </StyledIcon>
+        </button>
+      </ButtonGroup>
+    </HeaderContainer>
+  );
+};
+
+export default Header;
